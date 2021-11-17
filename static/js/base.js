31d8f0d616c907e33d163175a67a0645f0678ae7
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-17 10:36:40 +0000

    Added a dropdown menu to the login/register button

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,10 @@
 window.onscroll = function() {stickyNavbar()};
 
 let navbar = document.getElementById("navbar");
-let navbarBrand = document.getElementById("navbar-brand")
-let navbarSearch = document.getElementById("navbar-search")
-let navbarMenu = document.getElementById("navbar-menu")
-let navbarIcons = document.getElementById("navbar-icons")
+let navbarBrand = document.getElementById("navbar-brand");
+let navbarSearch = document.getElementById("navbar-search");
+let navbarMenu = document.getElementById("navbar-menu");
+let navbarIcons = document.getElementById("navbar-icons");
 let navbarMobile = document.getElementById("navbar-mobile");
 let sticky = navbar.offsetHeight;
 
@@ -18,8 +18,11 @@ let currentMainPost = mainPosts[postIndex];
 
 let progressInterval = setInterval(progress, 100); // 180
 
+/* Enable/Disable dropdown menu for navbar login */
+document.getElementById("navbar-login-text").onclick = function() {loginFunction()};
+
 /* The following Javascript was originally written by W3Schools (https://www.w3schools.com/howto/howto_js_navbar_sticky.asp)
-    and repurposed to aid in the function of the sticky menu */
+   and repurposed to aid in the function of the sticky menu */
 function stickyNavbar() {
     
     if (window.pageYOffset >= sticky) {
@@ -40,7 +43,7 @@ function stickyNavbar() {
 }
 
 /* This function was originally written by W3Schools (https://www.w3schools.com/howto/howto_js_mobile_navbar.asp) and
-    repurposed to aid in the function of the mobile menu */
+   repurposed to aid in the function of the mobile menu */
 function mobileMenu() {
     if (navbarMobile.style.display === "none") {
         navbarMobile.style.display = "grid";
@@ -48,6 +51,9 @@ function mobileMenu() {
         navbarMobile.style.display = "none";
     }
 }
+
+/* The following Carousel code was originally written by Juan Codepen.io (https://codepen.io/J0hn03cb1ac8a4/pen/MWYeQqp)
+   and repurposed to aid in the function of the carousel */
 
 function progress() {
   if (i === 100) {
@@ -82,4 +88,9 @@ function progress() {
     currentMainPost.classList.add("main-post--active");
     currentMainPost.classList.remove("main-post--not-active");
   }
+}
+
+/* Click function for the login dropdown menu */
+function loginFunction() {
+  document.getElementById("navbar-login-dropdown").classList.toggle("show-dropdown");
 }
