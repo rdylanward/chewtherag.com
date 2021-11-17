660bc8533abf4e70e04ecde47ecce7733bff80f8
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-17 13:37:44 +0000

    Added the about page

--- about/views.py
+++ about/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 
 def about(request):
-    """ A view to return the about.html page """
+    """ A view to return the 'About Us' page """
 
     return render(request, 'about/about.html')
