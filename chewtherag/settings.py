d0fae3742593c492ceb908e03b936a5569c236ad
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-21 16:38:36 +0000

    Added the collection app to the relevant Django files

--- chewtherag/settings.py
+++ chewtherag/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'about',
     'contact',
     'community',
+    'collection',
 ]
 
 MIDDLEWARE = [
--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def collection(request):
+    """ A view to return the 'Community' page """
+
+    return render(request, 'collection/collection.html')
