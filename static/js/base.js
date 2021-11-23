288ef79c4c167964e5e8919e94ffa1d0598109f2
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-23 21:07:49 +0000

    Updated the sticky navbar and login dropdown menu js

--- static/js/base.js
+++ static/js/base.js
@@ -1,33 +1,49 @@
-window.onscroll = function() {stickyNavbar()};
+$(document).ready(function() {
+    /* Initialise variables */
+    let navbar_height = $(".container-navbar").height();
+    let navbar_offset = $(".container-navbar").offset();
+    let sticky = navbar_offset.top + navbar_height;
 
-let navbar = document.getElementById("navbar");
-let navbarBrand = document.getElementById("navbar-brand");
-let navbarSearch = document.getElementById("navbar-search");
-let navbarMenu = document.getElementById("navbar-menu");
-let navbarIcons = document.getElementById("navbar-icons");
-let navbarMobile = document.getElementById("navbar-mobile");
-let sticky = navbar.offsetHeight;
+    let login_pos = $(".navbar-login-text").position();
+    let login_left = login_pos.left;
+    let login_height = $(".navbar-login-text").outerHeight()
+    let login_top = login_pos.top + login_height;
 
-/* The following Javascript was originally written by W3Schools (https://www.w3schools.com/howto/howto_js_navbar_sticky.asp)
-   and repurposed to aid in the function of the sticky menu */
-function stickyNavbar() {
-    
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky-navbar");
-        navbarBrand.classList.add("sticky-navbar-brand");
-        navbarSearch.classList.add("sticky-navbar-search");
-        navbarMenu.classList.add("sticky-navbar-menu");
-        navbarIcons.classList.add("sticky-navbar-icons");
-        navbarMobile.classList.add("sticky-navbar-mobile");
-    } else {
-        navbar.classList.remove("sticky-navbar");
-        navbarBrand.classList.remove("sticky-navbar-brand");
-        navbarSearch.classList.remove("sticky-navbar-search");
-        navbarMenu.classList.remove("sticky-navbar-menu");
-        navbarIcons.classList.remove("sticky-navbar-icons");
-        navbarMobile.classList.remove("sticky-navbar-mobile");
-    }
-}
+    /* Enable/Disable the sticky menu */
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= sticky) {
+            $(".container-navbar").addClass("sticky-navbar");
+            $(".navbar-brand").addClass("sticky-navbar-brand");
+            $(".navbar-menu").addClass("sticky-navbar-menu");
+            $(".navbar-search").addClass("sticky-navbar-search");
+            $(".navbar-icons").addClass("sticky-navbar-icons");
+            $(".navbar-mobile").addClass("sticky-navbar-mobile");
+        } else {
+            $(".container-navbar").removeClass("sticky-navbar");
+            $(".navbar-brand").removeClass("sticky-navbar-brand");
+            $(".navbar-menu").removeClass("sticky-navbar-menu");
+            $(".navbar-search").removeClass("sticky-navbar-search");
+            $(".navbar-icons").removeClass("sticky-navbar-icons");
+            $(".navbar-mobile").removeClass("sticky-navbar-mobile");
+        }
+    })
+
+    /* Enable/Disable the login dropdown menu */
+    $(".navbar-login-text").mouseover(function(){
+        $(".navbar-login-dropdown").css("left", login_left);
+        $(".navbar-login-dropdown").css("top", login_top);
+        $(".navbar-login-dropdown").css("display", "block");
+    }).mouseout(function(){        
+        $(".navbar-login-dropdown").css("display", "none");    
+    })
+
+    /* Keep the login dropdown menu open while in focus*/
+    $(".navbar-login-dropdown").mouseover(function(){
+        $(".navbar-login-dropdown").css("display", "block");
+    }).mouseout(function(){        
+        $(".navbar-login-dropdown").css("display", "none");    
+    })
+});
 
 /* This function was originally written by W3Schools (https://www.w3schools.com/howto/howto_js_mobile_navbar.asp) and
    repurposed to aid in the function of the mobile menu */
@@ -39,18 +55,6 @@ function mobileMenu() {
     }
 }
 
-/* Enable/Disable dropdown menu for navbar login */
+/* Enable/Disable dropdown menu for navbar login
 $(document).ready(function() {
-    $('.navbar-login-text').mouseenter(function(){
-        let height = $(".navbar-login-text").height();
-        let offset = $(".navbar-login-text").offset();
-        let left = offset.left;
-        let top = (offset.top + height + 2);
-
-        $(".navbar-login-dropdown").left(left);
-        $(".navbar-login-dropdown").top(top);
-        $(".navbar-login-dropdown").display("block");
-    }).mouseleave(function(){        
-        $(".navbar-login-dropdown").display("none");    
-    })
-});
+}); */
