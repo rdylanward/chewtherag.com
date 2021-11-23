20b38981c4045cdfac4eabb00828242970a6e490
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-23 00:18:19 +0000

    Removed unused js code and updated navbar

--- static/js/base.js
+++ static/js/base.js
@@ -8,16 +8,6 @@ let navbarIcons = document.getElementById("navbar-icons");
 let navbarMobile = document.getElementById("navbar-mobile");
 let sticky = navbar.offsetHeight;
 
-let mainPosts = document.querySelectorAll(".main-post");
-let posts = document.querySelectorAll(".post");
-
-let i = 0;
-let postIndex = 0;
-let currentPost = posts[postIndex];
-let currentMainPost = mainPosts[postIndex];
-
-let progressInterval = setInterval(progress, 100); // 180
-
 /* Enable/Disable dropdown menu for navbar login */
 document.getElementById("navbar-login-text").onclick = function() {loginFunction()};
 
@@ -50,44 +40,6 @@ function mobileMenu() {
     } else {
         navbarMobile.style.display = "none";
     }
-}
-
-/* The following Carousel code was originally written by Juan Codepen.io (https://codepen.io/J0hn03cb1ac8a4/pen/MWYeQqp)
-   and repurposed to aid in the function of the carousel */
-
-function progress() {
-  if (i === 100) {
-    i = -5;
-    // reset progress bar
-    currentPost.querySelector(".progress-bar__fill").style.width = 0;
-    document.querySelector(
-      ".progress-bar--primary .progress-bar__fill"
-    ).style.width = 0;
-    currentPost.classList.remove("post--active");
-
-    postIndex++;
-
-    currentMainPost.classList.add("main-post--not-active");
-    currentMainPost.classList.remove("main-post--active");
-
-    // reset postIndex to loop over the slides again
-    if (postIndex === posts.length) {
-      postIndex = 0;
-    }
-
-    currentPost = posts[postIndex];
-    currentMainPost = mainPosts[postIndex];
-  } else {
-    i++;
-    currentPost.querySelector(".progress-bar__fill").style.width = `${i}%`;
-    document.querySelector(
-      ".progress-bar--primary .progress-bar__fill"
-    ).style.width = `${i}%`;
-    currentPost.classList.add("post--active");
-
-    currentMainPost.classList.add("main-post--active");
-    currentMainPost.classList.remove("main-post--not-active");
-  }
 }
 
 /* Click function for the login dropdown menu */
