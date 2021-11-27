727c652279651a6ad74c4df5d3c42b17e433449e
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-27 15:05:46 +0000

    Customised admin

--- collection/admin.py
+++ collection/admin.py
@@ -3,5 +3,24 @@ from .models import Item, Collection
 
 
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(Collection)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'collection',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Collection, CollectionAdmin)
