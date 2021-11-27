28a19a4cdc141a0caf91483da531a174d7bc4fa3
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-27 15:16:26 +0000

    Added items to the collection app

--- collection/views.py
+++ collection/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
 def collection(request):
-    """ A view to return the 'Community' page """
+    """ A view to return the 'Community' page, view all items and search queries """
 
-    return render(request, 'collection/collection.html')
+    items = Item.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'collection/collection.html', context)
