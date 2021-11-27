b25874c0b8b0b299be70a20e026211eddb47617b
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-27 14:20:55 +0000

    Added items and collections for the collection app, recreated media folder

--- chewtherag/settings.py
+++ chewtherag/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',  # Required by Allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
