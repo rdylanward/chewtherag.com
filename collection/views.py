52bcb803f69e7746472794dcab6aa178026bb370
Dylan Ward <r.dylan.ward@gmail.com>
2021-11-28 17:28:18 +0000

    Initiallise the search function

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Item
 
 # Create your views here.
@@ -8,9 +10,21 @@ def collection(request):
     """ A view to return the 'Community' page, view all items and search queries """
 
     items = Item.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('collection'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            items = items.filter(queries)
 
     context = {
         'items': items,
+        'search_term': query,
     }
 
     return render(request, 'collection/collection.html', context)
